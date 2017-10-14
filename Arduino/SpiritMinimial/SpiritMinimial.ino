b9dccaa929f5f6366290e6a23f70e8d562b471e2
Eric Banker <ebanker@gmail.com>
2017-10-14 14:45:02 -0400

    now that I've built the minimal spirit circuit here is the updated code and diagram. 5v out on the buck converter

--- Arduino/SpiritMinimial/SpiritMinimial.ino
+++ Arduino/SpiritMinimial/SpiritMinimial.ino
@@ -1,7 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
 #define NEO_POWER 2 // for cyclotron and powercell
-Adafruit_NeoPixel powerStick = Adafruit_NeoPixel(44, NEO_POWER, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel powerStick = Adafruit_NeoPixel(16, NEO_POWER, NEO_GRB + NEO_KHZ800);
+
+#define NEO_CYCLO 3 // for cyclotron
+Adafruit_NeoPixel cyclotron = Adafruit_NeoPixel(4, NEO_CYCLO, NEO_GRB + NEO_KHZ800);
 
 // ##############################
 // available options
@@ -13,11 +16,15 @@ void setup() {
   powerStick.begin();
   powerStick.setBrightness(80);
   powerStick.show(); // Initialize all pixels to 'off'
+
+  cyclotron.begin();
+  cyclotron.setBrightness(80);
+  cyclotron.show(); // Initialize all pixels to 'off'
 }
 
 int pwr_interval = 60;       // interval at which to cycle lights for the powercell.
 int cyc_interval = 1000;      // interval at which to cycle lights for the cyclotron.
-int cyc_fade_interval = 15;   // fade the inactive cyclotron to light to nothing
+int cyc_fade_interval = 1;   // fade the inactive cyclotron to light to nothing
 
 void loop() {
   // get the current time
@@ -31,26 +38,26 @@ void setCyclotronLightState(int startLed, int endLed, int state ){
   switch ( state ) {
     case 0:
       for(int i=startLed; i <= endLed; i++) { // red
-        powerStick.setPixelColor(i, powerStick.Color(150, 0, 0));
+        cyclotron.setPixelColor(i, powerStick.Color(255, 0, 0));
       }
       break;
     case 1:
       for(int i=startLed; i <= endLed; i++) { // orange
-        powerStick.setPixelColor(i, powerStick.Color(255, 106, 0));
+        cyclotron.setPixelColor(i, powerStick.Color(255, 106, 0));
       }
       break;
     case 2:
       for(int i=startLed; i <= endLed; i++) { // off
-        powerStick.setPixelColor(i, 0);
+        cyclotron.setPixelColor(i, 0);
       }
       break;
     case 3:
       for(int i=startLed; i <= endLed; i++) {
         if( cyclotronRunningFadeOut >= 0 ){
-          powerStick.setPixelColor(i, 255 * cyclotronRunningFadeOut/255, 0, 0);
+          cyclotron.setPixelColor(i, 255 * cyclotronRunningFadeOut/255, 0, 0);
           cyclotronRunningFadeOut--;
         }else{
-          powerStick.setPixelColor(i, 0);
+          cyclotron.setPixelColor(i, 0);
         }
       }
       break;
@@ -58,20 +65,20 @@ void setCyclotronLightState(int startLed, int endLed, int state ){
 }
 
 /*************** Powercell/Cyclotron Animations *********************/
-int c1Start = 16;
-int c1End = 22;
-int c2Start = 23;
-int c2End = 29;
-int c3Start = 30;
-int c3End = 36;
-int c4Start = 37;
-int c4End = 44;
+int c1Start = 0;
+int c1End = 0;
+int c2Start = 1;
+int c2End = 1;
+int c3Start = 2;
+int c3End = 2;
+int c4Start = 3;
+int c4End = 3;
 
 unsigned long prevPwrMillis = 0;        // last time we changed a powercell light in the idle sequence
 unsigned long prevCycMillis = 0;        // last time we changed a cyclotron light in the idle sequence
 unsigned long prevFadeCycMillis = 0;    // last time we changed a fading cyclotron light in the idle sequence
 
-int powerSeqTotal = 15;       // total number of led's for powercell 0 based
+int powerSeqTotal = 14;       // total number of led's for powercell 0 based
 int powerSeqNum = 0;          // current running powercell sequence led
 
 int cycOrder = 0;
@@ -80,6 +87,7 @@ int cycFading = -1;
 // normal animation on the bar graph
 void powerSequenceOne(int currentMillis, int anispeed, int cycspeed, int cycfadespeed) {
   bool doUpdate = false;
+  bool doCycUpdate = false;
   
   // START CYCLOTRON 
   if( useGameCyclotronEffect == true ){
@@ -122,7 +130,7 @@ void powerSequenceOne(int currentMillis, int anispeed, int cycspeed, int cycfade
           break;
       }
   
-      doUpdate = true;
+      doCycUpdate = true;
     }
   
     // now figure out the fading light
@@ -143,7 +151,7 @@ void powerSequenceOne(int currentMillis, int anispeed, int cycspeed, int cycfade
             setCyclotronLightState(c3Start, c3End, 3);
             break;
         }
-        doUpdate = true;
+        doCycUpdate = true;
       }
     }
   }else{
@@ -190,16 +198,20 @@ void powerSequenceOne(int currentMillis, int anispeed, int cycspeed, int cycfade
           break;
       }
   
-      doUpdate = true;
+      doCycUpdate = true;
     }
   }
+  
+  if( doCycUpdate == true ){
+    cyclotron.show(); // send to the neopixels
+  }
   // END CYCLOTRON
+  
   // START POWERCELL
   if (currentMillis - prevPwrMillis > anispeed) {
     // save the last time you blinked the LED
     prevPwrMillis = currentMillis;
 
-    
     for ( int i = 0; i <= powerSeqTotal; i++) {
       if ( i <= powerSeqNum ) {
         powerStick.setPixelColor(i, powerStick.Color(0, 0, 150));
